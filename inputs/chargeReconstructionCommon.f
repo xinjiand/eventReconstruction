97b070daa6d430673304772fcdb633e2145c1bd4
xinjiand <xinjiand27@gmail.com>
2017-12-27 00:10:54 -0500

    Add files via upload

---
+++ inputs/chargeReconstructionCommon.f
@@ -0,0 +1,16 @@
+        common nphoton(200,200,6),nvertex(6)
+        common ntp(200,200,6,400)
+        common plane(6,100),ntrack,ncube
+        common rscint,rgap,rshield,rgfilm,pmtreflect
+        common pi,epsilon,finity,clight
+        common celldim,pmtr,pcyield,relativeyield,qe,tfilm,tgfilm,glength,ncombine,gztrans,nmirror
+        common ascint,afilm,agfilm,agbuffer,agrcoef
+        common nalost,nflost,ntrapped,nerror,npmtreturn
+        common ngblost,ngflost,ngtlost,nsuccess,ngrlost,ngerror,ngreflect,ngstart,nwithincrit,ngplanes,ngdp,ngpmtp
+        common ndtransits,ngtransits,dpath,gpath,dtpath,ntbin,ntprint
+        common nxoffset,nyoffset,nzoffset
+        common eff(15),sb(15)
+        common ecell(25,25,25) !this sets maximum ncube to 25
+        common photon(200,200,6),dummy(200,200,6)
+        common nlogx(1000),nlogy(1000),nlogz(1000),plog(1000),nptr(1000),de(6),grad(1000),tlog(1000),dlog(1000)
+        common tnlogx(1000),tnlogy(1000),tnlogz(1000),tplog(1000)
---
+++ inputs/trans-common.f
@@ -0,0 +1,12 @@
+	common nphoton(200,200,6),nvertex(6)
+	common ntp(200,200,6,400)
+	common plane(6,100),ntrack,ncube
+	common rscint,rgap,rshield,rgfilm,pmtreflect,npmtcouple
+	common pi,epsilon,finity,clight
+	common celldim,pmtr,pcyield,relativeyield,qe,tfilm,tgfilm,glength,ncombine,gztrans
+	common ascint,afilm,agfilm,agbuffer,agrcoef
+	common nalost,nflost,ntrapped,nerror,npmtreturn
+	common ngblost,ngflost,ngtlost,nsuccess,ngrlost,ngerror,ngreflect,ngstart,nwithincrit,ngplanes,ngdp,ngpmtp
+	common ndtransits,ngtransits,dpath,gpath,dtpath,ntbin,ntprint
+	common nxoffset,nyoffset,nzoffset
+	
